b3631638657eb0fb5c76d0ae93a368b1ec8352d5
Anwesh43 <anweshthecool0@gmail.com>
2018-04-07 08:18:58 +0530

    adding Animator class

--- cornerballmoverview/src/main/java/ui/anwesome/com/cornerballmoverview/CornerBallMoverView.kt
+++ cornerballmoverview/src/main/java/ui/anwesome/com/cornerballmoverview/CornerBallMoverView.kt
@@ -25,4 +25,27 @@ class CornerBallMoverView (ctx : Context) : View(ctx) {
         }
         return true
     }
+
+    data class State (var dir : Float = 0f, var prevScale : Float = 0f, var j : Int = 0) {
+
+        val scales : Array<Float> = arrayOf(0f, 0f, 0f)
+
+        fun update (stopcb : () -> Unit) {
+            scales[j] += 0.1f * dir
+            if (Math.abs(scales[j] - prevScale) > 1) {
+                scales[j] = prevScale + dir
+                j += dir.toInt()
+                if (j == scales.size) {
+                    stopcb()
+                }
+            }
+        }
+
+        fun startUpdating(startcb : () -> Unit) {
+            if (dir == 0f) {
+                dir = 1 - 2 * prevScale
+                startcb()
+            }
+        }
+    }
 }
